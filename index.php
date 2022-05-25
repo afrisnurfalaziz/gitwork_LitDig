4b9855664be456c29ad6efa2a5443656dad2840f
Afris Nurfal aziz <74905155+afrisnurfalaziz@users.noreply.github.com>
2022-05-25 10:31:26 +0700

    Update Git Literasi Digital

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git Literasi Digital</title>
+</head>
+
+<body>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus quae, atque eligendi unde quaerat itaque, sapiente reiciendis velit aperiam, dolor id deserunt eum voluptate placeat! Dolor nemo molestiae ipsam magni?
+</body>
+
+</html>
